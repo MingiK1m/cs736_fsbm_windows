dd5a7443fa8a2eb9d44cca998e2054b7e1996386
Mingi Kim <ksmk99@gmail.com>
2016-12-08 12:22:21 -0600

    Modify run_fsbm.bat to repeat only 10 times
    1. The batch file repeats only 10 times.
    2. No more echo when execute batch file.

--- Debug/run_fsbm.bat
+++ Debug/run_fsbm.bat
@@ -1,3 +1,4 @@
+@echo off
 REM 
 REM This is very similar to the linux benchmarking things.
 REM Only thing different from it is that this doesn't use filepath argument.
@@ -5,8 +6,8 @@ REM
 
 REM for large file microbenchmark
 REM 1KB to 1MB
-for %%i in (1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576) do fs_microbenchmark.exe large %%i 1
+for %%i in (1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576) do fs_microbenchmark.exe large %%i 10
 
 REM for small file microbenchmark
 REM 1KB to 128KB <-- because it takes too long to make 10000 1MB files in HDD. 
-for %%i in (1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072) do fs_microbenchmark.exe small %%i 1
+for %%i in (1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072) do fs_microbenchmark.exe small %%i 10
